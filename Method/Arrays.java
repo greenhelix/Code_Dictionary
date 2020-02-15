06eee473d8bd82f394a4abf48a83a1971221e077
Ik Hwan <dlrghks4444@gmail.com>
2020-02-15 15:55:56 +0900

    class modify

--- Method/Arrays.java
+++ Method/Arrays.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
-class Method {
+class Array {
 
     public static void main(String[] args) {
         int[] b = { 1, 2, 3 };
