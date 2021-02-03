4edf90f8f9aaef1dea5c209bfca9e6855b89f6bc
greenhelix <dlrghks4444@gmail.com>
2021-02-03 19:35:07 +0900

    리스트 거꾸로 변환 메서드

---
+++ Python_Snippet/list_reverse.py
@@ -0,0 +1,21 @@
+# List reverse method -
+A = [0, 10, 20, 40]
+A.reverse()
+
+print(A)    # [40, 20, 10, 0]
+
+b = [1, 2, 4, 3, 5]
+# [5, 3, 4, 2, 1] You need to assign a new variable.
+print(list(reversed(b)))
+print(reversed(b))  # <list_reverseiterator object at 0x000002436A50D148>
+
+array = [0, 10, 20, 40]
+for i in reversed(array):
+    print(i)
+
+'''
+40
+20
+10
+0
+'''
