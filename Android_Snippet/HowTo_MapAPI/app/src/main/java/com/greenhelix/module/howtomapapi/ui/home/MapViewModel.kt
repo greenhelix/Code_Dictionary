dcfdc39d4e2f6083bf4b6c09de1e939e3c8ce0a1
Kevin <dlrghks4444@gmail.com>
2022-05-15 00:27:20 +0900

    viewmodel에서 onmapready callback 함수를 구현하는 것은 의미가 없다 생각되었다. 왜냐하면 onmapready 함수에서는 context를 요구하는 경우가 많은데, viewmodel에서 context를 끌어다 사용하면 사실상 아키텍처의 의미가 떨어지기 때문에, 사용을 안하기로 했다.
    추가적으로 마커를 클릭하면 풍선 모양으로 정보창이 뜨는 것을 추가했다.
    - 이부분을 좀더 카드형식이나 간단한 UI로 커스텀해보는 것도 좋을 듯하다.
    
    - 아니면 마커를 클릭시 정보창을 어떻게 띄울지 생각해보는 것도 좋겠다.

--- Android_Snippet/HowTo_MapAPI/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/MapViewModel.kt
+++ Android_Snippet/HowTo_MapAPI/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/MapViewModel.kt
@@ -1,70 +1,13 @@
 package com.greenhelix.module.howtomapapi.ui.home
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.graphics.Color
-import android.location.Location
-import android.location.LocationListener
-import android.util.Log
-import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.util.FusedLocationSource
-import com.naver.maps.map.util.MarkerIcons
 
-class MapViewModel : ViewModel(), OnMapReadyCallback  {
+class MapViewModel : ViewModel()  {
+
+
+}
 
 //    private val _text = MutableLiveData<String>().apply {
 //        value = "This is home Fragment"
 //    }
 //    val text: LiveData<String> = _text
-    private lateinit var locationSource: FusedLocationSource
-    private lateinit var  fusedLocationProviderClient: FusedLocationProviderClient
-
-    fun getMyPos(fragment: NaverMapFragment){
-        Log.d("ik", "getMyPos")
-        locationSource = FusedLocationSource(fragment, 1000 )
-    }
-
-    // 지도에 표기한 옵션이나 객체를 동기화 해주는 함수이다.
-    fun mapAsync(mapFrag : MapFragment?){
-        mapFrag?.getMapAsync(this)
-    }
-
-    override fun onMapReady(naverMap: NaverMap) {
-        Log.d("ik", "mapOptions")
-
-        naverMap.locationSource = locationSource
-
-        naverMap.cameraPosition = CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 37.0, 45.0)
-
-        naverMap.uiSettings.isCompassEnabled = true
-
-        naverMap.uiSettings.isLocationButtonEnabled = true
-
-        Marker().apply {
-            position = LatLng(37.5670135, 126.9783740)
-            map = naverMap
-        }
-
-        Marker().apply {
-            position = LatLng(37.57000, 126.97618)
-            icon = MarkerIcons.BLACK
-            angle = 315f
-            map = naverMap
-        }
-
-        Marker().apply {
-            position = LatLng(37.56500, 126.9783881)
-            icon = MarkerIcons.BLACK
-            iconTintColor = Color.RED
-            alpha = 0.5f
-            map = naverMap
-        }
-
-
-    }
-}
--- Android_Snippet/HowTo_MapAPI/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/NaverMapFragment.kt
+++ Android_Snippet/HowTo_MapAPI/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/NaverMapFragment.kt
@@ -1,5 +1,6 @@
 package com.greenhelix.module.howtomapapi.ui.home
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -11,13 +12,23 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.greenhelix.module.howtomapapi.R
 import com.greenhelix.module.howtomapapi.databinding.FragmentNaverMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
-class NaverMapFragment : Fragment(){
+class NaverMapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapViewModel: MapViewModel
     private var _binding: FragmentNaverMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var locationSource: FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,14 +66,17 @@ class NaverMapFragment : Fragment(){
             childFragmentManager.beginTransaction().add(R.id.map_naver_frag, this).commit()
         }
 
-        mapViewModel.mapAsync(mapFragment)
+        locationSource = FusedLocationSource(this, 1000 )
 
-        mapViewModel.getMyPos(this)
+
+
+        mapFragment.getMapAsync(this)
+
 
         return root
     }
 
-    val permissionRequest = registerForActivityResult(
+    private val permissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()){ permission ->
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             when {
@@ -76,6 +90,68 @@ class NaverMapFragment : Fragment(){
                 }
             }
         }
+    }
+
+
+    override fun onMapReady(naverMap: NaverMap) {
+        Log.d("ik", "mapOptions")
+        val infoWindow = InfoWindow()
+
+        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(requireContext()){
+            override fun getText(info: InfoWindow): CharSequence {
+                return "정보창 내용"
+            }
+        }
+
+        val listener = Overlay.OnClickListener { overlay ->
+            val marker = overlay as Marker
+
+            if (marker.infoWindow == null) {
+                // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                infoWindow.open(marker)
+            } else {
+                // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                infoWindow.close()
+            }
+
+            true
+        }
+
+        naverMap.locationSource = locationSource
+
+        naverMap.cameraPosition = CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 37.0, 45.0)
+
+        naverMap.uiSettings.isCompassEnabled = true
+
+        naverMap.uiSettings.isLocationButtonEnabled = true
+
+        Marker().apply {
+            position = LatLng(37.5670135, 126.9783740)
+            map = naverMap
+            this.onClickListener = listener
+        }
+
+        Marker().apply {
+            position = LatLng(37.57000, 126.97618)
+            icon = MarkerIcons.BLACK
+            angle = 315f
+            map = naverMap
+            this.onClickListener = listener
+        }
+
+        Marker().apply {
+            position = LatLng(37.56500, 126.9783881)
+            icon = MarkerIcons.BLACK
+            iconTintColor = Color.RED
+            alpha = 0.5f
+            map = naverMap
+            this.onClickListener = listener
+        }
+
+        naverMap.setOnMapClickListener { pointF, latLng ->
+            infoWindow.close()
+        }
+
     }
 
     override fun onDestroyView() {
