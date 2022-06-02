10368de8bb7fcbeea9f12d83a2a8301c1d98ea2a
KostaIoT_ik <dlrghks4444@gmail.com>
2022-06-02 14:40:24 +0900

    Drive 파일 변경

--- Java_Snippet/Drive.java
+++ Java_Snippet/Drive.java
@@ -1,68 +1,87 @@
-public class Drive {
-    public static void main(String[] args) {
-        Car car = new Car();
+import java.util.Scanner;
 
-    }
+public class Drive {
+	public static int speed = 0;
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// System.out.println();
+		
+		Car car = new Car();
+		Driver driver = new Driver();
+		
+		car.engineStart();
+		int sec = 0;
+		while(car.state){
+			
+			
+			if(speed == 30) break;
+		}
+
+	}
 }
 
 interface Engine {
 
 }
 
-interface Driver {
-    public void licenseKind();
+class Driver {
+	
+	Driver(){}
+	
+	public String licenseKind(int kind)
+	{
+		switch(kind){
+			case 1: return "Manual";
+			case 2: return "Auto";
+			default: return "no license!";
+		}
+	}
 
-    public void changeGear();
+	public int gearChange(int gearState)
+	{
+		switch (gearState) {
+			case 1:
+				System.out.println("Gear 1");
+				break;
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
 
-    public void accelerator();
+			default:
+				System.out.println("Gear 0");
+				break;
+		}
+		return gear;
+	}
+
+	public void accelerator()
+	{
+	   speed++;
+	}
 
 }
 
-class Car implements Engine, Driver {
-    int gear = 0;
-    int speed = 0;
+class Car {
+	int gear = 0;
+	
+	boolean state = false;
+	
+	Car() {
+	    System.out.println("no person in the car");
+	}
+	
+	Car(Driver person){
+	    System.out.println("person in the car");
+	}
 
-    Car() {
-    }
-
-    public void engineStart() {
-        System.out.println("Engine Start");
-    }
-
-    public int gearChange(int gearState) {
-
-        switch (gearState) {
-            case 1:
-                System.out.println("Gear 1");
-                break;
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-
-            default:
-                System.out.println("Gear 0");
-                break;
-        }
-        return gear;
-    }
-
-    @Override
-    public void licenseKind() {
-
-    }
-
-    @Override
-    public void changeGear() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void accelerator() {
-        // TODO Auto-generated method stub
-
-    }
+	public void engineStart() 
+	{
+		System.out.println("Engine Start");
+		state = true;
+		System.out.println("This car is ");
+	}
 
 }
